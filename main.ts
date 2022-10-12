e80790cb60568cdf518eac340e14af40b80c4597
happyboy2725 <115052103+happyboy2725@users.noreply.github.com>
2022-10-12 09:35:36 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,54 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    count = 10
+    while (count >= 1) {
+        count += -1
+        basic.showNumber(count)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
+input.onButtonPressed(Button.B, function () {
+    count = 0
+})
+let count = 0
+basic.showString("2D22Jovani")
